d6b4d3db6a487425cf54edabe16d5baa22ee8f1f
Peter Seibel <peter@gigamonkeys.com>
2021-11-26 13:02:51 -0800

    Basic movement working with snake object.

--- snake.lua
+++ snake.lua
@@ -6,21 +6,31 @@
 
 function _init()
   rectfill(0, 0, 128, 128, 13)
-  x = 64
-  y = 64
+  move(snake)
 end
 
 function _update()
-  if (btn(0)) snake.direction = LEFT
-  if (btn(1)) snake.direction = RIGHT
-  if (btn(2)) snake.direction = UP
-  if (btn(3)) snake.direction = DOWN
-  x += snake.direction.dx * snake.speed
-  y += snake.direction.dy * snake.speed
+  if (btnp(0)) then
+    add(snake.turns, LEFT)
+  end
+  if (btnp(1)) then
+    add(snake.turns, RIGHT)
+  end
+  if (btnp(2)) then
+    add(snake.turns, UP)
+  end
+  if (btnp(3)) then
+    add(snake.turns, DOWN)
+  end
+  move(snake)
 end
 
 function _draw()
-  spr(1, (x \ 8) * 8, (y \ 8) * 8)
+  local h = heads[snake.direction]
+  local hd = head(snake)
+  --print("X: " .. hd.x * 8 .. "; Y: " .. hd.y * 8 .. "; h: " .. h)
+
+  spr(h, hd.x, hd.y)
 end
 
 function to_cell(i)
@@ -31,6 +41,56 @@ function to_index(cell)
   return cell.y * 8 + cell.x
 end
 
+
+function head(snake)
+  return snake.segments[(snake.head + 63) % 64]
+end
+
+function current_cell(snake)
+  return { x = (snake.x \ 8) * 8, y = (snake.y \ 8) * 8 }
+end
+
+function apply_next_turn(snake)
+  while #snake.turns > 0 do
+    t = snake.turns[1]
+    deli(snake.turns, 1)
+    if legal_turn(snake.direction, t) then
+      snake.direction = t
+      break
+    end
+  end
+end
+
+function legal_turn(d, t)
+  return d.dx == t.dy or d.dy == t.dx
+end
+
+function move(snake)
+  snake.x += snake.direction.dx * snake.speed
+  snake.y += snake.direction.dy * snake.speed
+  -- draw head with correct sprite
+  local current = current_cell(snake)
+  local head = head(snake)
+  --print("C X: " .. current.x .. " Y: " .. current.y .. ". H X: " .. head.x .. "; Y: " .. head.y)
+
+  if (current.x ~= head.x or current.y ~= head.y) then
+    -- Entering new cell.
+
+    -- print("Setting head")
+    -- print("C X: " .. current.x .. " Y: " .. current.y .. ". H X: " .. head.x .. "; Y: " .. head.y)
+    --print("new cell")
+
+    apply_next_turn(snake)
+
+    spr(1, head.x, head.y)
+    snake.segments[(snake.head + 63) % 64] = current
+  else
+    --print("same cell")
+  end
+end
+
+
+
 SIZE = 8 -- 128px display / 16px sprites.
 
 UP    = { dx = 0, dy = -1 }
@@ -38,10 +98,16 @@ DOWN  = { dx = 0, dy = 1 }
 LEFT  = { dx = -1, dy = 0 }
 RIGHT = { dx = 1, dy = 0 }
 
+-- sprites for the head.
+heads = { [UP] = 13, [DOWN] = 14, [LEFT] = 15, [RIGHT] = 16 }
+
 snake = {
   direction = RIGHT,
-  speed = 2,
+  speed = 1,
+  x = 64,
+  y = 64,
+  turns = {},
   head = 1,
   tail = 0,
-  segments = { [0] = to_index({x = 8, y = 8}) }
+  segments = { [0] = {x = 64, y = 64} }
 }
